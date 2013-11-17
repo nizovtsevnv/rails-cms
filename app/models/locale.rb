e2c22753f4d1f437ff25c425dc922de872822043
Nick Nizovtsev <nizovtsevnv@gmail.com>
2013-11-17 22:26:43 +0400

    Fix foreign key

--- app/models/locale.rb
+++ app/models/locale.rb
@@ -1,7 +1,7 @@
 class Locale < ActiveRecord::Base
   self.primary_key = :name
   
-  has_many :pages, :dependent => :destroy
+  has_many :pages, :dependent => :destroy, :class_name => Content::Page, :foreign_key => :locale_name
   
   def to_s
     name
