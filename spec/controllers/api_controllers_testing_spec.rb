0f2904b7f3afc6d2a43f6bc15408634a2d98e877
Nick Nizovtsev <nizovtsevnv@gmail.com>
2013-11-17 22:26:03 +0400

    Change specs for automatic test controllers

--- spec/controllers/api_controllers_testing_spec.rb
+++ spec/controllers/api_controllers_testing_spec.rb
@@ -6,35 +6,53 @@ describe '@data in' do
     'api/content/pages' => {:factory => :content_page, :routes => [:create, :destroy, :index, :show, :update]},
     'api/content/uploads' => {:factory => :content_upload, :routes => [:create, :destroy, :index, :update]},
     'api/content/widgets' => {:factory => :content_widget, :routes => [:create, :destroy, :index, :show, :update]},
-    'api/locales' => {:factory => :locale, :routes => [:create, :destroy, :index, :show, :update]},
+    'api/locales' => {:factory => :locale, :id => :name, :routes => [:create, :destroy, :index, :show, :update]},
     'api/users' => {:factory => :user, :routes => [:create, :destroy, :index, :show, :update]}
   }.each do |controller, options|
+    options[:id] ||= :id 
     describe "#{controller}_controller".camelize.safe_constantize do
       before(:each){ @object = FactoryGirl.create options[:factory] }
       options[:routes].each do |action|
         context "#{controller}\##{action} must be" do
         case action
         when :create
-          it "#{options[:factory].to_s.camelize} object" do
-            get :create
-            expect(assigns(:data)).to eq(@object)
+          # on the input: params hash[factory] = {attribute: value, ...}
+          # on the output: @data = last persisted Object, @success = true
+          it "#{options[:factory].to_s.camelize} object and be succesful" do
+            post :create, options[:factory] => FactoryGirl.attributes_for(options[:factory])
+            expect(assigns(:data)).to eq(@object.class.last)
+            expect(assigns(:success)).to be true
           end
         when :destroy
-          it "true" do
-            get :destroy, :id => @object.id
-            expect(assigns(:data)).to be true
+          # on the input: params hash[:id] = some_id
+          # on the output: @success = true
+          it "#{options[:factory].to_s.camelize} object and be succesful" do
+            get :destroy, :id => @object[options[:id]]
+            expect(assigns(:success)).to be true
           end
         when :index
+          # on the input: params hash with optional keys for sorting, filtering and pagination
+          # on the output: @data = ActiveRecord::Relation(Object), @total = Object.count
           it "an ActiveRecord::Relation object" do
             get :index
             expect(assigns(:data)).to eq([@object])
+            expect(assigns(:total)).to eq(@object.class.count)
           end
         when :show
+          # on the input: params hash[:id] = some_id
+          # on the output: @data = Object
           it "#{options[:factory].to_s.camelize} object" do
-            get :show, :id => @object.id
+            get :show, :id => @object[options[:id]]
             expect(assigns(:data)).to eq(@object)
           end
         when :update
+          # on the input: params hash with id and new values
+          # on the output: @data = Object, @success = true
+          it "#{options[:factory].to_s.camelize} object and be succesful" do
+            post :update, :id => @object[options[:id]], options[:factory] => FactoryGirl.attributes_for(options[:factory])
+            expect(assigns(:data)).to eq(@object)
+            expect(assigns(:success)).to be true
+          end
         end
         end
       end
