aa9aa460c60860fa38e23febd6fc7a1aceabd3a2
Nick Nizovtsev <nizovtsevnv@gmail.com>
2013-11-18 22:43:19 +0400

    Add blank parts for cms js app

---
+++ app/controllers/cms_controller.rb
@@ -0,0 +1,6 @@
+class CmsController < ApplicationController
+  layout 'cms'
+  
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Cms::Application.routes.draw do
   root :to => 'pages#show', :name => nil
-  
+
+  get 'cms' => 'cms#show'
   get 'pages' => 'pages#index'
   get 'pages/:name' => 'pages#show', :as => :page, :constraints => { :name => /.+/ }
 
