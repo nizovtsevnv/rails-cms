ca491cd30593bcd453245a1d5bedd070a05b7665
Nick Nizovtsev <nizovtsevnv@gmail.com>
2013-11-18 22:29:29 +0400

    Spec for autotest controllers refactoring

--- spec/controllers/api_controllers_testing_spec.rb
+++ spec/controllers/api_controllers_testing_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe 'Request to' do
   {
     'api/activities' => {:factory => :activity, :routes => [:index, :show]},
-    'api/content/pages' => {:factory => :content_page, :routes => [:create, :destroy, :index, :show, :update]},
+    'api/content/pages' => {:factory => :content_page, :routes => [:create, :destroy, :index, :show, :update], :params => {:create => [:name, :locale_name]}},
     'api/content/uploads' => {:factory => :content_upload, :routes => [:create, :destroy, :index, :update]},
-    'api/content/widgets' => {:factory => :content_widget, :routes => [:create, :destroy, :index, :show, :update]},
+    'api/content/widgets' => {:factory => :content_widget, :routes => [:create, :destroy, :index, :show, :update], :params => {:create => [:name, :locale_name]}},
     'api/locales' => {:factory => :locale, :id => :name, :routes => [:create, :destroy, :index, :show, :update]},
     'api/users' => {:factory => :user, :routes => [:create, :destroy, :index, :show, :update]}
   }.each do |controller, options|
@@ -19,10 +19,16 @@ describe 'Request to' do
           # on the input: params hash[factory] = {attribute: value, ...}
           # on the output: @data = last persisted Object, @success = true
           when :create
-            before(:each){
-              values = FactoryGirl.attributes_for(options[:factory])
+            before{
+              values = if options[:params] && options[:params][:create]
+                new_object = FactoryGirl.build(options[:factory])
+                Hash[options[:params][:create].map{|p| [p, new_object.send(p)]}]
+              else
+                FactoryGirl.attributes_for(options[:factory])
+              end
               post :create, options[:factory] => values
             }
+            
             it "create record without errors in @data" do
               expect(assigns(:data).errors.messages).to eq({})
             end
@@ -38,29 +44,37 @@ describe 'Request to' do
           # on the input: params hash[:id] = some_id
           # on the output: @success = true
           when :destroy
-            it "define @success = true" do
+            before{
               get :destroy, :id => @object[options[:id]]
+            }
+            
+            it "define @success = true" do
               expect(assigns(:success)).to be true
             end
 
           # on the input: params hash with optional keys for sorting, filtering and pagination
           # on the output: @data = ActiveRecord::Relation(Object), @total = Object.count
           when :index
-            it "define a list of model records in @data" do
+            before{
               get :index
+            }
+
+            it "define a list of model records in @data" do
               expect(assigns(:data)).to eq([@object])
             end
 
             it "define count of model records in @total" do
-              get :index
               expect(assigns(:total)).to be(@object.class.count)
             end
 
           # on the input: params hash[:id] = some_id
           # on the output: @data = Object
           when :show
-            it "define selected model record in @data" do
+            before{
               get :show, :id => @object[options[:id]]
+            }
+            
+            it "define selected model record in @data" do
               expect(assigns(:data)).to eq(@object)
             end
 
@@ -68,9 +82,15 @@ describe 'Request to' do
           # on the output: @data = Object, @success = true
           when :update
             before(:each){
-              new_values = FactoryGirl.attributes_for(options[:factory])
-              post :update, :id => @object[options[:id]], options[:factory] => new_values
+              values = if options[:params] && options[:params][:create]
+                new_object = FactoryGirl.build(options[:factory])
+                Hash[options[:params][:create].map{|p| [p, new_object.send(p)]}]
+              else
+                FactoryGirl.attributes_for(options[:factory])
+              end
+              post :update, :id => @object[options[:id]], options[:factory] => values
             }
+            
             it "update attributes without errors in @data" do
               expect(assigns(:data).errors.messages).to eq({})
             end
